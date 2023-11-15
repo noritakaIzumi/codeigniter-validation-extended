34ad3fe64fb77340e34ea0cad53e6fe54b4a9f47
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-11-15 20:40:15 +0900

    modify test class

--- Tests/ValidationTest.php
+++ Tests/ValidationTest.php
@@ -1,35 +1,42 @@
 <?php
 
-namespace Testsa;
+namespace Tests;
 
 use CodeIgniter\Validation\Validation;
 use Config\Services as AppServices;
 use Config\Validation as ValidationConfig;
 use PHPUnit\Framework\TestCase;
+use Validator\ValidatorRulesCreator;
 
 /**
  * @covers \Validator\ValidatorRulesCreator
  */
 class ValidationTest extends TestCase
 {
-    protected static ?Validation $orgValidator;
+    protected static Validation $orgValidator;
+    protected static ValidatorRulesCreator $orgRulesCreator;
+    protected ?Validation $validation;
+    protected ?ValidatorRulesCreator $rulesCreator;
 
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
         self::$orgValidator = new Validation(config(ValidationConfig::class), AppServices::renderer());
+        self::$orgRulesCreator = new ValidatorRulesCreator();
     }
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->validator = clone self::$orgValidator;
+        $this->validation = clone self::$orgValidator;
+        $this->rulesCreator = clone self::$orgRulesCreator;
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-        $this->validator = null;
+        $this->validation = null;
+        $this->rulesCreator = null;
     }
 
     public function testSample(): void
