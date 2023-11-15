fd0bfa261cba93a2d03acff3d24f1c2a9d63f656
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-11-15 20:45:56 +0900

    change class name

--- Tests/ValidationTest.php
+++ Tests/ValidationTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection GrazieInspection */
 
 namespace Tests;
 
@@ -6,23 +6,23 @@ use CodeIgniter\Validation\Validation;
 use Config\Services as AppServices;
 use Config\Validation as ValidationConfig;
 use PHPUnit\Framework\TestCase;
-use Validator\ValidatorRulesCreator;
+use Validator\RulesCreator;
 
 /**
- * @covers \Validator\ValidatorRulesCreator
+ * @covers \Validator\RulesCreator
  */
 class ValidationTest extends TestCase
 {
     protected static Validation $orgValidator;
-    protected static ValidatorRulesCreator $orgRulesCreator;
+    protected static RulesCreator $orgRulesCreator;
     protected ?Validation $validation;
-    protected ?ValidatorRulesCreator $rulesCreator;
+    protected ?RulesCreator $rulesCreator;
 
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
         self::$orgValidator = new Validation(config(ValidationConfig::class), AppServices::renderer());
-        self::$orgRulesCreator = new ValidatorRulesCreator();
+        self::$orgRulesCreator = new RulesCreator();
     }
 
     protected function setUp(): void
--- Validator/RulesCreator.php
+++ Validator/RulesCreator.php
@@ -6,7 +6,7 @@ namespace Validator;
  * CodeIgniter のバリデーターに渡すルールの配列を生成するクラス。
  * @see https://codeigniter.com/user_guide/libraries/validation.html#:~:text=Or%20as%20a%20labeled%20style%3A
  */
-class ValidatorRulesCreator
+class RulesCreator
 {
 
 }
