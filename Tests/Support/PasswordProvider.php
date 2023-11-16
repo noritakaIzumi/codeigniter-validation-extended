0344155b936c911a888a3b6aa5a350df099c5b94
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-11-16 18:46:25 +0900

    test: password is required and8 chars at least and255 chars at most and alphanumeric punctual

---
+++ Tests/Support/PasswordProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Support;
+
+readonly class PasswordProvider
+{
+    /**
+     * @param array $data an array of data to validate
+     * @param bool $isValid expected result of validator
+     * @param string $message message if test fails
+     */
+    public function __construct(public array $data, public bool $isValid, public string $message)
+    {
+    }
+}
--- Tests/ValidationTest.php
+++ Tests/ValidationTest.php
@@ -5,6 +5,7 @@ namespace Tests;
 use CodeIgniter\Config\Services;
 use CodeIgniter\Validation\Validation;
 use PHPUnit\Framework\TestCase;
+use Tests\Support\PasswordProvider;
 use Tests\Support\UsernameProvider;
 use Validator\Field;
 use Validator\Rules;
@@ -67,6 +68,41 @@ class ValidationTest extends TestCase
         $rules = new Rules();
         $rules->required()->minLength(3)->maxLength(30);
         $field = new Field(name: 'username', label: 'Username', rules: $rules);
+        $this->rulesCreator->addField($field);
+
+        $this->validation->setRules($this->rulesCreator->export());
+
+        $this->assertSame($provider->isValid, $this->validation->run($provider->data), $provider->message);
+    }
+
+    public static function passwordProvider(): array
+    {
+        return [
+            [new PasswordProvider([], false, 'password is missing')],
+            [new PasswordProvider(['password' => ''], false, 'empty password')],
+            [new PasswordProvider(['password' => str_repeat('a', 7)], false, 'less than 8 chars')],
+            [new PasswordProvider(['password' => str_repeat('a', 8)], true, 'exact 8 chars')],
+            [new PasswordProvider(['password' => str_repeat('a', 255)], true, 'exact 255 chars')],
+            [new PasswordProvider(['password' => 'Password'], true, 'contains alphanumeric')],
+            [new PasswordProvider(['password' => '~!#$%&*-_+=|:.'], true, 'contains figures')],
+            [new PasswordProvider(['password' => '////////'], false, 'prohibited figures included')],
+        ];
+    }
+
+    /**
+     * It tests:
+     * ```
+     * $validation->setRule('password', 'Password', ['required', 'max_length[255]', 'min_length[8]', 'alpha_numeric_punct']);
+     * ```
+     * @dataProvider passwordProvider
+     * @param PasswordProvider $provider
+     * @return void
+     */
+    public function testPasswordIsRequiredAnd8CharsAtLeastAnd255CharsAtMostAndAlphaNumericPunctual(PasswordProvider $provider): void
+    {
+        $rules = new Rules();
+        $rules->required()->minLength(8)->maxLength(255)->alphaNumericPunct();
+        $field = new Field(name: 'password', label: 'Password', rules: $rules);
         $this->rulesCreator->addField($field);
 
         $this->validation->setRules($this->rulesCreator->export());
